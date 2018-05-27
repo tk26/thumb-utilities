8adb57062c5495e0f974cb85bb8d215a1148b7f7
jdromine <jromines@gmail.com>
2018-05-27 13:42:44 -0400

    Exporting location class

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const GeoPoint = require('./domain/location_services/GeoPoint.js');
 const TripBoundary = require('./domain/location_services/TripBoundary.js');
+const Location = require('./domain/location_services/Location.js');
 
 module.exports = {
   GeoPoint: GeoPoint,
-  TripBoundary: TripBoundary
+  TripBoundary: TripBoundary,
+  Location: Location
 };
