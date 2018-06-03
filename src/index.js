7638ff27f026cc4823f147c0152916da75f5b41f
Tejas Kumthekar <tk@Tejass-MacBook-Pro.local>
2018-06-03 17:27:25 -0500

    Expose User

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ const TripBoundary = require('./domain/location_services/TripBoundary.js');
 const Location = require('./domain/location_services/Location.js');
 const RiderInvitation = require('./domain/trip_services/RiderInvitation.js');
 const DriverInvitation = require('./domain/trip_services/DriverInvitation.js');
+const User = require('./validation/User.js');
 
 module.exports = {
   GeoPoint: GeoPoint,
   TripBoundary: TripBoundary,
   Location: Location,
   RiderInvitation: RiderInvitation,
-  DriverInvitation: DriverInvitation
+  DriverInvitation: DriverInvitation,
+  User: User,
 };
